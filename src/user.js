9ed40e5a85e53a15cbc3fac36bb0dbee248fee5a
Boris <boris260692@gmail.com>
2022-05-19 23:31:22 +0300

    Change index.html

--- src/user.js
+++ src/user.js
@@ -1,3 +1,20 @@
 let cars = ['Honda','BMW','Nissan']
 
-console.log(cars)
+let upperCase = cars.map( car => car.toUpperCase() )
+console.log(upperCase)
+// let newCar = cars.indexOf('BMW')
+// cars[newCar] = 'Opel'
+// console.log(cars)
+
+// let people = [
+//     {name: 'Elena', budget: 3900},
+//     {name: 'Boris', budget: 4600},
+//     {name: 'Victoria', budget: 2800}
+// ]
+
+// // let person = people.find(function(person){
+// //     return person.budget === 2800
+// // })
+
+// let person = people.find(person => person.budget === 4600)
+// console.log(person)
