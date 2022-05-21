4d7be01f373e805743ec25559d649ebe42b6a29b
Boris <boris260692@gmail.com>
2022-05-21 19:48:26 +0300

    Change index.html

--- src/user.js
+++ src/user.js
@@ -1,29 +1,9 @@
-// let cars = ['Honda','BMW','Nissan']
-// let numbers = [1,2,3,5,7,10,15,20]
-// let filtredNumbers = numbers.filter( num => num > 10 )
-// console.log(filtredNumbers)
-// console.log(numbers)
-// let upperCase = cars.map( car => car.toUpperCase() )
-// console.log(upperCase)
-// // let newCar = cars.indexOf('BMW')
-// // cars[newCar] = 'Opel'
-// // console.log(cars)
-
-let people = [
-    {name: 'Elena', budget: 3900},
-    {name: 'Boris', budget: 4600},
-    {name: 'Victoria', budget: 2800}
-]
-
-let allBudget = people.filter( person => person.budget > 3000).reduce( (acc,person) => {
-    acc += person.budget
-    return acc
-}, 0 )
-console.log(allBudget)
-
-// // let person = people.find(function(person){
-// //     return person.budget === 2800
-// // })
-
-// let person = people.find(person => person.budget === 4600)
-// console.log(person)
+let person = {
+    name: 'Boris',
+    age: 29,
+    isProgrammer: true,
+    languages:['UK','EN','RU'],
+    greet: function(){
+        console.log(`Hellow mr.${person.name}`);
+    }
+}
