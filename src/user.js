142aa0623455f89c3fb4ea423b347a3a09d1b0a7
Boris <boris260692@gmail.com>
2022-05-24 22:06:57 +0300

    Change index.html

--- src/user.js
+++ src/user.js
@@ -1,15 +1,37 @@
-let person = {
-    name: 'Boris',
-    age: 29,
-    isProgrammer: true,
-    languages:['UK','EN','RU'],
-    greet(){
-        console.log(`Hellow mr.${person.name}`);
-    },
-}
-for (let key in person) {
-    if(person[key] === 29){
-        console.log(key + ' ' + person[key])
-        console.log(person.greet())
-    }
-}
+// let userSec = prompt('Введіть кількість секунд')
+
+// let init = () => {
+//     sec = 0;
+//     setInterval(tick, 1000);
+// }
+// // clearInterval(init)
+// function tick(){
+//     if (sec < userSec){
+//         sec++;
+//     } else {
+//         return clearInterval(init)
+//     }
+//     console.log(sec)
+// }
+// init()
+
+// function dispHide () {
+//     setTimeout(dispShow, 2000)
+// }
+// function dispShow (){
+//     var element = document.querySelectorAll('.accordion-item');
+//     console.log(element)
+//     for (let elem of element){
+//         elem.classList.add("active_block")
+//     }
+// }
+// dispHide()
+// let someElem = document.getElementsByTagName('li')
+// for (let p of someElem){
+//     p.style.color = 'blue'
+// }
+// let titText = document.querySelector('.tit_text_learn')
+// titText.textContent = "NEW text"
+// titText.style.color = 'red'
+// titText.style.fontSize = '40px'
+// titText.style.background = 'yellow'
