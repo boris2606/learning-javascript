37013f4daa28dfee0516cec38675bfdac9a0cf48
Boris <boris260692@gmail.com>
2022-05-25 22:55:00 +0300

    Create prod

--- src/user.js
+++ src/user.js
@@ -31,7 +31,37 @@
 //     p.style.color = 'blue'
 // }
 // let titText = document.querySelector('.tit_text_learn')
-// titText.textContent = "NEW text"
-// titText.style.color = 'red'
-// titText.style.fontSize = '40px'
-// titText.style.background = 'yellow'
+
+// function addStyles (element,text,color,bg){
+//     element.textContent = text
+//     element.style.color = color
+//     element.style.background = bg
+// }
+// setTimeout(() => {
+//     addStyles(titText, 'Перший текст', 'green', 'black')
+// },2000)
+
+// setTimeout(() => {
+//     addStyles(titText, 'Другий текст', 'red' ,' green')
+// },3000)
+
+// setTimeout(() => {
+//     addStyles(titText, 'Третій текст', 'blue' , 'yellow')
+// },4000)
+let clickExz = document.querySelector('.click_exz')
+clickExz.onclick = () =>{
+    if( clickExz.style.color === 'black'){
+        clickExz.style.color = 'blue'
+    } else {
+        clickExz.style.color = 'black'
+    }
+}
+
+let clickExz2 = document.querySelector('.click_exz2')
+clickExz2.addEventListener('dblclick', () => {
+    if( clickExz2.style.color === 'black'){
+        clickExz2.style.color = 'blue'
+    } else {
+        clickExz2.style.color = 'black'
+    }
+})
